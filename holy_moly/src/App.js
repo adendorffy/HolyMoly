87b86e7a02fceff946194e3ad65f690b306c60ff
adendorffy <[24051055@sun.ac.za]>
2024-08-22 09:24:17 +0200

    font family and colors

--- holy_moly/src/App.js
+++ holy_moly/src/App.js
@@ -20,21 +20,63 @@ const App = () => {
     setModalOpen(false);
   };
 
+  const weightedRandom = () => {
+    const weights = [
+      { value: 0.5, weight: 0.3 }, // 30% chance
+      { value: 1, weight: 0.2 }, // 20% chance
+      { value: 2, weight: 0.2 }, // 20% chance
+      { value: 3, weight: 0.05 }, // 5% chance
+      { value: 4, weight: 0.05 }, // 5% chance
+      { value: 5, weight: 0.05 }, // 5% chance
+      { value: 6, weight: 0.05 }, // 5% chance
+      { value: 7, weight: 0.025 }, // 2.5% chance
+      { value: 8, weight: 0.025 }, // 2.5% chance
+      { value: 9, weight: 0.025 }, // 2.5% chance
+      { value: 10, weight: 0.025 }, // 2.5% chance
+      { value: 11, weight: 0.01 }, // 1% chance
+      { value: 12, weight: 0.01 }, // 1% chance
+      { value: 13, weight: 0.01 }, // 1% chance
+      { value: 14, weight: 0.01 }, // 1% chance
+      { value: 15, weight: 0.01 }, // 1% chance
+    ];
+
+    const totalWeight = weights.reduce(
+      (acc, current) => acc + current.weight,
+      0
+    );
+    const random = Math.random() * totalWeight;
+
+    let accumulatedWeight = 0;
+
+    for (let i = 0; i < weights.length; i++) {
+      accumulatedWeight += weights[i].weight;
+      if (random < accumulatedWeight) {
+        return weights[i].value;
+      }
+    }
+  };
+
   const handlePredict = () => {
-    const randomNum = Math.floor(Math.random() * 100);
+    let randomNum = weightedRandom();
+    console.log("Random weighted number:", randomNum);
     setRandomNumber(randomNum);
     setModalOpen(true);
   };
 
   return (
     <div className="App">
-      <h1>Holy Moly</h1>
+      <div className="app-title">Holy Moly</div>
+      {!photo && (
+        <div className="brown-text">
+          removing potholes one bag of asphalt at a time
+        </div>
+      )}
       <PhotoUpload
         onPhotoUploaded={handlePhotoUploaded}
         onPredict={handlePredict}
       />
       <Modal isOpen={isModalOpen} onClose={closeModal}>
-        You need {randomNumber} bags
+        You need <p className="orange-text">{randomNumber}</p> bags
       </Modal>
     </div>
   );
